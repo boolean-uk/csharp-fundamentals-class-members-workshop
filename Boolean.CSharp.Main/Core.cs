7468bc3e53b236a20d67cda8588f461e4d17a57e
uerbzr <nigel.sibbert@gmail.com>
2023-04-03 15:13:36 +0100

    Add project files.

---
+++ Boolean.CSharp.Main/Core.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Boolean.CSharp.Main
+{
+    public class Core  // sealed // partial // abstract
+    {
+        //public members
+        //private members - prefix _underscore
+
+
+
+
+    }
+}
